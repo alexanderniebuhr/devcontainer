c17fcec8b724bbf23f388814765c1d79da6249f0
Alexander Niebuhr <tech@nbhr.io>
2022-04-03 21:16:32 +0200

    chore: :tada: first commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# The install.sh script is the installation entrypoint for any dev container 'features' in this repository.
+#
+# The tooling will parse the devcontainer-features.json + user devcontainer, and write
+# any build-time arguments into a feature-set scoped "devcontainer-features.env"
+# The author is free to source that file and use it however they would like.
+set -a
+. ./devcontainer-features.env
+set +a
+
+
+if [ ! -z ${_BUILD_ARG_VOLTA} ]; then
+    curl https://get.volta.sh | bash
+fi
